7f5fa96798eeee38e67af6c9b45f1a3d0a256ac3
Juan José Arreola Gonzalez <juanjosearreola99@gmail.com>
2024-04-18 14:42:01 -0600

    BluetoothData

--- Bluetooth_clasico/app/src/main/java/com/plcoding/bluetoothchat/domain/chat/ConnectionResult.kt
+++ Bluetooth_clasico/app/src/main/java/com/plcoding/bluetoothchat/domain/chat/ConnectionResult.kt
@@ -2,5 +2,6 @@ package com.plcoding.bluetoothchat.domain.chat
 
 sealed interface ConnectionResult {
     object ConnectionEstablished: ConnectionResult
+    data class TransferSuccess(val message: BluetoothMessage): ConnectionResult
     data class Error(val message: String): ConnectionResult
 }
